df6234c13da3fd233e8103cdbe293857d146e1bc
Tsuyoshi <inoue0406@gmail.com>
2018-09-30 16:50:21 +0900

    modified to make correct train/valid split indices

---
+++ src/main_typhoon_withcsv_whl.py
@@ -0,0 +1,149 @@
+#
+# Typhoon Classification Task (Signate 2018)
+# Using 'fastai' library and take csv as input
+#
+# whl: Main routine for running the whole dataset
+#
+import time
+import os
+import sys
+
+from fastai.imports import *
+
+from fastai.transforms import *
+from fastai.conv_learner import *
+from fastai.model import *
+from fastai.dataset import *
+from fastai.sgdr import *
+from fastai.plots import *
+
+#from sklearn.metrics import recall_score, precision_score
+
+def recall(preds,targs):
+    th = 0.5
+    prob_TC = np.exp(preds[:,0])
+    flg = 1 * (prob_TC > th)
+    t2 = targs.cpu().numpy() # ground truth
+    f2 = flg.numpy()         # predicted
+    TP = sum(t2*f2)
+    FN = sum(t2*(1-f2))
+    return TP/(TP+FN)
+
+def precision(preds,targs):
+    th = 0.5
+    prob_TC = np.exp(preds[:,0])
+    flg = 1 * (prob_TC > th)
+    t2 = targs.cpu().numpy() # ground truth
+    f2 = flg.numpy()         # predicted
+    TP = sum(t2*f2)
+    FP = sum((1-t2)*f2)
+    return TP/(TP+FP)
+
+from opts import *
+
+if __name__ == '__main__':
+    # parse command-line options
+    opt = parse_opts()
+    print(opt)
+
+    # create result dir
+    if not os.path.exists(opt.result_path):
+        os.mkdir(opt.result_path)
+    
+    with open(os.path.join(opt.result_path, 'opts.json'), 'w') as opt_file:
+        json.dump(vars(opt), opt_file)
+
+    # generic log file
+    logfile = open(os.path.join(opt.result_path, 'log_run.txt'),'w')
+    logfile.write('Start time:'+time.ctime()+'\n')
+    tstart = time.time()
+
+    # image size
+    sz = opt.size_img
+
+    PATH = "../data/"
+
+    # Neural Net Architecture
+    if opt.network == 'resnet34':
+        f_model = resnet34
+    elif opt.network == 'resnet50':
+        f_model = resnet50
+    elif opt.network == 'resnet152':
+        f_model = resnet152
+    elif opt.network == 'resnext50':
+        f_model = resnext50
+    elif opt.network == 'vgg19':
+        f_model = vgg19
+    elif opt.network == 'inceptionresnet_2':
+        f_model = inceptionresnet_2
+
+    # Transformations
+    tfms = tfms_from_model(f_model, sz, aug_tfms=transforms_top_down, max_zoom=1.05)
+    
+    # Data loader
+    print('setting data loader')
+    
+    n = len(list(open(opt.train_flist)))-1
+    print('training data number:',n)
+    # validation indices from csv file
+    #val_idxs = get_cv_idxs(n)
+    df_idx_whole = pd.read_csv('../data/valid_idx_whole_v2.csv',header=False, index=False)
+    val_idxs = df_idx_whole['valid_idx'].values.tolist()
+    
+    data = ImageClassifierData.from_csv(path=PATH, folder='train_orig/',
+                                        csv_fname=opt.train_flist, bs=opt.batch_size,
+                                        tfms=tfms, val_idxs=val_idxs,
+                                        test_name='test/', num_workers=6)
+    #
+    #metrics = [accuracy,recall,precision]
+    metrics = [accuracy]
+    learn = ConvLearner.pretrained(f_model, data, precompute=False, metrics=metrics)
+    #learn = ConvLearner.pretrained(f_model, data, precompute=False)
+
+    # learning rate finder
+    flg_finder = False
+    if flg_finder == True:
+        print('learning rate finder')
+        learn.lr_find()
+        # save result of lr finder
+        df_lr = pd.DataFrame({ 'lr' : learn.sched.lrs,
+                               'loss' : learn.sched.losses})
+        df_lr.to_csv('%s/lr_finder.csv' % (opt.result_path))
+
+    # from lr finder
+    #lr = 0.001
+    lr = opt.learning_rate
+    # differential learning rate
+    lfac = opt.lr_diff
+    lrs = np.array([lr/(lfac**2),lr/lfac,lr])
+
+    print('start fitting')
+    learn.unfreeze()
+    learn.fit(lrs, opt.n_cycles, cycle_len=1, cycle_mult=2)
+
+    # save the trained model
+    learn.save(opt.result_path)
+
+    # test with TTA
+    print('test with TTA')
+    multi_preds, y = learn.TTA(is_test=True)
+    preds = np.mean(multi_preds, 0)
+    prob_TC = np.exp(preds[:,0])
+
+    test_fnames = learn.data.test_ds.fnames
+    # remove 'test/'
+    test_fnames2 = [str.replace('test/','') for str in test_fnames]
+
+    for th in [0.4,0.5,0.6,0.7,0.8,0.9,0.97,0.99]:
+        flg = 1 * (prob_TC > th)
+        df = pd.DataFrame({ 'fname' : test_fnames2,
+                            'pred' : flg})
+        df = df.sort_values('fname')
+        df.to_csv('%s/test_pred_fastai_th%4.2f.tsv' % (opt.result_path,th), header=False, index=False, sep='\t')
+        
+    # output elapsed time
+    logfile.write('End time: '+time.ctime()+'\n')
+    tend = time.time()
+    tdiff = float(tend-tstart)/3600.0
+    logfile.write('Elapsed time[hours]: %f \n' % tdiff)
+
--- src_prep/prep_label_whole.py
+++ src_prep/prep_label_whole.py
@@ -2,8 +2,27 @@
 # Prepare label csv for classification
 #
 import pandas as pd
+import numpy as np
 import os
 import random
+
+def get_cv_idxs(n, cv_idx=0, val_pct=0.2, seed=42):
+    """ Get a list of index values for Validation set from a dataset
+    
+    Arguments:
+        n : int, Total number of elements in the data set.
+        cv_idx : int, starting index [idx_start = cv_idx*int(val_pct*n)] 
+        val_pct : (int, float), validation set percentage 
+        seed : seed value for RandomState
+        
+    Returns:
+        list of indexes 
+    """
+    np.random.seed(seed)
+    n_val = int(val_pct*n)
+    idx_start = cv_idx*n_val
+    idxs = np.random.permutation(n)
+    return idxs[idx_start:idx_start+n_val]
     
 if __name__ == '__main__':
     # fixed seed
@@ -26,17 +45,41 @@ if __name__ == '__main__':
     print('test data size',len(files_te))
     print(files_te[0])
 
+    # training / validaton split 20% validation
+    id_va_po = sorted(get_cv_idxs(len(files_po)))
+    id_va_ng = sorted(get_cv_idxs(len(files_ng)))
+
+    files_po_va = [files_po[x] for x in id_va_po]
+    files_po_tr = [files_po[x] for x in list(set(range(len(files_po)))-set(id_va_po))]
+    files_ng_va = [files_ng[x] for x in id_va_ng]
+    files_ng_tr = [files_ng[x] for x in list(set(range(len(files_ng)))-set(id_va_ng))]
+
     # resample positve instances to match negative ones
-    Ntr = len(files_ng)
+    files_ng_lst = [files_ng_tr,files_ng_va]
+    files_po_lst = [files_po_tr,files_po_va]
+
+    df_list = []
+    for i in range(2):
+        files_ng = files_ng_lst[i]
+        files_po = files_po_lst[i]
+        Ntr = len(files_ng)
     
-    ng_slct = files_ng
-    ng_slct2 = ['nonTC/'+x for x in ng_slct]
+        ng_slct = files_ng
+        ng_slct2 = ['nonTC/'+x for x in ng_slct]
 
-    # resample
-    po_slct = random.choices(files_po,k=Ntr)
-    po_slct2 = ['TC/'+x for x in po_slct]
+        # resample
+        po_slct = random.choices(files_po,k=Ntr)
+        po_slct2 = ['TC/'+x for x in po_slct]
 
-    # save to file
-    df = pd.DataFrame({ 'fname' : po_slct2 + ng_slct2,
-                        'label' : ([1]*Ntr)+([0]*Ntr)})
-    df.to_csv('../data/label_list_whole.csv',header=False, index=False)
+        # save to file
+        df_list.append(pd.DataFrame({ 'fname' : po_slct2 + ng_slct2,
+                                      'label' : ([1]*Ntr)+([0]*Ntr)}))
+
+    df = pd.concat(df_list)
+    df.to_csv('../data/label_list_whole_v2.csv',header=False, index=False)
+    # validation label
+    valid_label =  ([0]*len(df_list[0])) + ([1]*len(df_list[1]))
+    valid_idx = np.where(valid_label)[0]
+    df_idx = pd.DataFrame({'valid_idx' :valid_idx})
+    df_idx.to_csv('../data/valid_idx_whole_v2.csv',header=False, index=False)
+
